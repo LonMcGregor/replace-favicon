c4fb72a1b9e0b85499b880bab8c34237a1bf39bb
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2019-01-23 18:51:08 +0000

    don't include a minified version - too much work

--- min/contentscript.js
+++
@@ -1,3 +0,0 @@
-
-/* Rational Anarchist License */
-function i(b){var l=document.createElement("link");l.id='replaceMissingFavicon';l.rel='icon';l.href=b;document.head.appendChild(l);};function p(){const n=window.location.protocol==="file:"?window.location.pathname:window.location.host;chrome.runtime.sendMessage({url:n},r=>{if(r.url===n){i(r.icon);}});}function m(){if(document.querySelectorAll("link[rel~='icon']").length>0){return;}if(window.location.protocol==="file:"){p();return;}fetch("favicon.ico").then(r=>{if(!r.ok){p();}});}m();
